d586d9920f2b15e5896cd9688c6369d817d82a73
HetrisleyGomes <hetrisleygomes@gmail.com>
2024-01-30 13:15:00 -0300

    Personalização do Spotify e outros ajustes

--- script.js
+++ script.js
@@ -1,21 +1,10 @@
 //BOM DIA | BOA TARDE | BOA NOITE
-
-// Obtém a referência do elemento com o ID "greeting"
 const greetingElement = document.getElementById("greeting");
 
-// Obtém a hora atual do sistema
+// Pegar a hora
 const currentHour = new Date().getHours();
 
-// Define a saudação com base na hora atual
-// if (currentHour >= 5 && currentHour < 12) {
-//   greetingElement.textContent = "Bom dia";
-// } else if (currentHour >= 12 && currentHour < 18) {
-//   greetingElement.textContent = "Boa tarde";
-// } else {
-//   greetingElement.textContent = "Boa noite";
-// }
-
-// Forma mais simples
+// Verifica a hora
 const greetingMessage =
   currentHour >= 5 && currentHour < 12
     ? "Bom dia"
@@ -30,13 +19,11 @@ const container = document.querySelector(".offer__list-item");
 
 const observer = new ResizeObserver(() => {  //mudanças no tamanho do elemento 
   const containerWidth = container.offsetWidth; //largura total do elemento, incluindo largura do conteúdo, bordas e preenchimento.
-  const numColumns = Math.floor(containerWidth / 200); //número de colunas com base na largura do container
+  const tamanhoVar = 250
+  const numColumns = Math.floor(containerWidth / tamanhoVar); //número de colunas com base na largura do container
 
-  //largura mínima de 200px e máxima de 1fr (uma fração do espaço disponível).
-  container.style.gridTemplateColumns = `repeat(${numColumns}, minmax(200px, 1fr))`;
-
-  console.log({ container });
-  console.log({ numColumns });
+  //largura mínima de 250px e máxima de 1fr (uma fração do espaço disponível).
+  container.style.gridTemplateColumns = `repeat(${numColumns}, minmax(${tamanhoVar}px, 1fr))`;
 });
 //observando a mudança do elemento
 observer.observe(container);
